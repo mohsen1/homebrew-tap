ff53331d70db4e86d9bd1f7d3f67139e4a17d92a
Mohsen Azimi <me@azimi.me>
2025-01-12 14:53:26 +0700

    feat(yek): auto-fetch latest version using GitHub API

--- Formula/yek.rb
+++ Formula/yek.rb
@@ -1,23 +1,48 @@
 class Yek < Formula
     desc "A tool to chunk and serialize repository content for LLM consumption"
     homepage "https://github.com/mohsen1/yek"
-    version "0.1.0"
+    version = :latest
+    url "https://api.github.com/repos/mohsen1/yek/releases/latest"
+    strategy :github_latest
+    head "https://github.com/mohsen1/yek.git", branch: "main"
   
-    if OS.mac?
+    on_macos do
       if Hardware::CPU.arm?
-        url "https://github.com/mohsen1/yek/releases/download/v#{version}/yek-aarch64-apple-darwin.tar.gz"
-        # sha256 will be added after first release
+        url do
+          {
+            url: "https://github.com/mohsen1/yek/releases/latest/download/yek-aarch64-apple-darwin.tar.gz",
+            strategy: :github_latest
+          }
+        end
       else
-        url "https://github.com/mohsen1/yek/releases/download/v#{version}/yek-x86_64-apple-darwin.tar.gz"
-        # sha256 will be added after first release
+        url do
+          {
+            url: "https://github.com/mohsen1/yek/releases/latest/download/yek-x86_64-apple-darwin.tar.gz",
+            strategy: :github_latest
+          }
+        end
       end
-    else
-      url "https://github.com/mohsen1/yek/releases/download/v#{version}/yek-x86_64-unknown-linux-gnu.tar.gz"
-      # sha256 will be added after first release
+    end
+  
+    on_linux do
+      url do
+        {
+          url: "https://github.com/mohsen1/yek/releases/latest/download/yek-x86_64-unknown-linux-gnu.tar.gz",
+          strategy: :github_latest
+        }
+      end
     end
   
     def install
-      bin.install "yek"
+      if OS.mac?
+        if Hardware::CPU.arm?
+          bin.install "yek-aarch64-apple-darwin/yek"
+        else
+          bin.install "yek-x86_64-apple-darwin/yek"
+        end
+      else
+        bin.install "yek-x86_64-unknown-linux-gnu/yek"
+      end
     end
   
     test do
